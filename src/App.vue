9df891ed0af41b9a851d46928fdc57c44bc2b2ea
frozenrunner <mike.horlick@gmail.com>
2022-12-29 09:33:59 -0500

    Update to Vue3

---
+++ src/App.vue
@@ -0,0 +1,141 @@
+<script setup>
+import { reactive } from 'vue'
+const title = "Picross";
+const imageData = reactive([]);
+let appImageHeight = 0;
+let appImageWidth = 0;
+
+function getColumnNumbers(n) {
+    n--; /*Vue for loop with integer values starts at 1*/
+    let count = 0;
+    let columnString = '';
+    for(let i = 0, len = appImageHeight; i < len; i++){
+        if (this.imageData[i][n] !== 'rgba(255,255,255,1)'){
+            count++;
+        }
+        else {
+            if (count > 0){
+                if (columnString.length > 0){
+                    columnString += '\r\n';
+                }
+                columnString += count.toString();
+                count = 0;
+            }
+        }
+    }
+    if (count > 0 && count < this.imageHeight) {
+        columnString = count.toString();
+    }
+    if (count === this.imageHeight){
+        columnString = count.toString();
+    }
+
+    return columnString;
+};
+
+function getRowNumbers(n) {
+    n--; /*Vue for loop with integer values starts at 1*/
+    let count = 0;
+    let rowString = '';
+    for(let i = 0, len = this.imageData[n].length; i < len; i++){
+        if (this.imageData[n][i] !== 'rgba(255,255,255,1)'){
+            count++;
+        }
+        else {
+            if (count > 0){
+                if (rowString.length > 0){
+                    rowString += '    ';
+                }
+                rowString += count.toString();
+                count = 0;
+            }
+        }
+    }
+    if (count > 0 && count < this.imageData[n].length) {
+        rowString = count.toString();
+    }
+    if (count === this.imageData[n].length){
+        rowString = count.toString();
+    }
+    return rowString;
+};
+
+function readImageData(event) {
+    let offset;
+    let depth = 4;
+    let fileList = event.target.files;
+    let fileReader = new FileReader();
+    fileReader.onload = function(event) {
+        let resultArray = new Uint8Array(event.target.result);
+
+        Module.__Z18allocateArraySpacei(event.target.result.byteLength);
+        offset = Module.__Z21getFileContentsOffsetv();
+
+        const outBuf = new Uint8Array(Module.buffer, offset, resultArray.length);
+        for (let i = 0; i < resultArray.length; i++){
+            outBuf[i] = resultArray[i];
+        }
+        Module.__Z16ProcessImageDatav();
+
+        let pixelVectorSize = Module.__Z18getPixelVectorSizev();
+        let pixelVectorStart = Module.__Z20getPixelVectorOffsetv();
+        let pixelData = new Uint8Array(Module.buffer, pixelVectorStart, depth*pixelVectorSize);
+        let imageWidth = Module.__Z13getImageWidthv();
+        let imageHeight = Module.__Z14getImageHeightv();
+         
+        for (let row = 0; row < imageHeight; row++) {
+            let rowData = [];
+            for (let col = 0; col < imageWidth; col++) {
+                let startingPoint = (row * imageWidth * depth) + (col * depth);
+                let opacity = pixelData[startingPoint];
+                let red = pixelData[startingPoint + 3];
+                let green = pixelData[startingPoint + 2];
+                let blue = pixelData[startingPoint + 1];
+
+                let rgbaString = "rgba(" +  red + "," + green + "," + blue;
+                if (opacity === 0 || (red === 255 && green === 255 && blue === 255)) {
+                    rgbaString = "rgba(255,255,255,1)";
+                }
+                else {
+                    opacity = 1;
+                    rgbaString += "," + opacity + ")";
+                }
+                console.log(rgbaString);
+                rowData.push(rgbaString);
+            }
+            imageData[row] = rowData;
+        }
+        let gameGrid = document.getElementById("gameContainer");
+        gameGrid.style.gridTemplateColumns = "repeat(" + imageWidth + ",1fr)";
+        gameGrid.style.gridTemplateRows = "repeat(" + imageHeight + ",1fr)";
+        appImageWidth = imageWidth;
+        appImageHeight = imageHeight;
+    }
+    fileReader.readAsArrayBuffer(fileList[0]);
+}
+</script>
+
+<template>
+  <input type="file" id="fileInput" @change="readImageData"/>
+  <h1>{{title}}</h1>
+    <div class="ui-main-container">
+        <div class="ui-column-numbers">
+            <div class="ui-number-hint ui-number-hint-column" v-for="n in appImageWidth">{{getColumnNumbers(n)}}</div>
+        </div>
+        <div class="ui-row-numbers">
+            <div class="ui-number-hint ui-number-hint-row" v-for="n in appImageHeight">{{getRowNumbers(n)}}</div>
+        </div>
+        <div id="gameContainer" class="ui-game-container">
+            <template v-for="imageRow in imageData">
+                <div class="ui-square-container material-icons" v-for="cell in imageRow" :style="{background: cell}"></div>
+            </template>
+        </div>
+        <div class="ui-button-container">
+                <div class="ui-button-cross" data-bind="css: { active: !checkActive() }, click: activateCross"><i class="material-icons">close</i></div>
+                <div class="ui-button-check" data-bind="css: { active: checkActive }, click: activateCheck"><i class="material-icons">check</i></div>
+            </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
